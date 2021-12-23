c51a3b9c5d78b488f0df7f42aaab58faabea9758
Random-Coder21 <96586454+Random-Coder21@users.noreply.github.com>
2021-12-23 11:14:06 -0500

    Created Storyboard for display

--- Bluetooth-Disabler/Bluetooth-Disabler/ViewController.swift
+++ Bluetooth-Disabler/Bluetooth-Disabler/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var control: UISegmentedControl!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    private func disableBluetooth() {
+        
+    }
 
-
+    @IBAction func controlChanged(_ sender: Any) {
+        if control.isEnabledForSegment(at: 1) {
+            disableBluetooth()
+        }
+    }
+    
 }
 
